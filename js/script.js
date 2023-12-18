eeebf7bd63ff8d555e1dcbc260c0c343519ccd1b
Carol Morais <carolmoraism12@gmail.com>
2023-12-18 20:12:47 -0300

    função para repassar parâmetros pela url para ativar opções em orçamento, refatoração de links

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-const links = document.querySelectorAll(".cabecalho-menu a");
+function initLinkAtivo() {
+  const links = document.querySelectorAll(".cabecalho-menu a");
+  if(links.length) {
 
-function ativarLink(link) {
-  const urlSite = window.location.href;
-  if(urlSite.includes(link.href)) {
-    link.classList.add("ativo");
+    function ativarLink(link) {
+      const urlSite = window.location.href;
+      if(urlSite.includes(link.href)) {
+        link.classList.add("ativo");
+      }
+    }
+    links.forEach(ativarLink)
   }
 }
 
 
-links.forEach(ativarLink)
+function ativarElementosByParametros() {
+  const parametros = new URLSearchParams(location.search);
+
+    parametros.forEach((item) => {
+      const elemento = document.getElementById(item);
+      if(elemento) {
+        elemento.checked = true;
+      }
+    })
+}
+
+initLinkAtivo();
+ativarElementosByParametros();
+
